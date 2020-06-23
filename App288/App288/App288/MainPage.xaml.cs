1cef8b699ef752f47e01b4799aa24d43b19e5941
XfHua <jasonHuaxf@163.com>
2020-06-23 15:09:07 +0800

    Add files via upload

---
+++ App288/App288/App288/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App288
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new ViewModel();
+        }
+    }
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel() {
+
+            path = "App288.ttt.png";
+        }
+
+        string path { get; set; }
+
+        public string Path
+        {
+            set
+            {
+                if (path != value)
+                {
+                    path = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Path"));
+                    }
+                }
+            }
+            get
+            {
+                return path;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+
+    public class stringToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ImageSource.FromResource(value as string, typeof(MainPage).GetTypeInfo().Assembly);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+}
